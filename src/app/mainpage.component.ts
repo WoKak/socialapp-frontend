ff250e7bccb83faeb1d5facdb848df6d885a7b1a
WoKak <michal.woj34@gmail.com>
2017-12-30 22:26:18 +0100

    file added to prev commit

--- src/app/mainpage.component.ts
+++ src/app/mainpage.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component ({
   selector: 'my-app',
-  template: '<br/>Main page',
+  template: `
+  <ul id="lst">
+    <li><a [routerLink] = "[\'/main\']">Main page</a></li>
+    <li><a [routerLink] = "[\'/profile\']">My profile</a></li>
+  </ul>
+  <br/>Main page`,
 })
-export   class   AppMainpage  {
-}
+export   class   AppMainpage  {}
